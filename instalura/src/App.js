959b71dc339f1d404a7fd139820c99cca0f1d2d5
Raul Guilherme Demonte Pontes <raul.pontes@microserviceit.com.br>
2020-01-23 08:21:35 -0300

    Commit inicial

---
+++ instalura/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Header from './componentes/Header';
+import Timeline from './componentes/Timeline';
+
+import './css/login.css';
+import './css/reset.css';
+import './css/timeline.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="root">
+        <div className="main">
+          <Header />
+          <Timeline />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
